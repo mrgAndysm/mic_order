46f76bdf9e183d243242afc3de804bae899f874a
gsq <13538462645@163.com>
2021-02-25 15:16:50 +0800

    debug

--- apps/routers/router.go
+++ apps/routers/router.go
@@ -1,12 +1,20 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mrgAndysm/mic_order/com/validata"
+)
 
 // InitRouters 初始化路由
 func InitRouters() *gin.Engine {
 	ginRouter := gin.Default()
 	ginRouter.GET("/order", func(context *gin.Context) {
-		context.String(200, "get orderinfos")
+		// context.String(200, "get orderinfos")
+		var json validata.TestData
+
+		context.ShouldBindJSON(&json)
+
+		context.String(200, "get orderinfos"+json.Name)
 	})
 
 	return ginRouter
---
+++ com/validata/test.go
@@ -0,0 +1,7 @@
+package validata
+
+// TestData Test
+type TestData struct {
+	Name string `json:"name"`
+	Age  int32  `json:"age"`
+}
